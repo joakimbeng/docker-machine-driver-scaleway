fc9e0f5721230584c420dbf92aa64a0bf16d6cc5
Joakim Carlstein <joakim@beng.se>
2018-03-15 15:13:21 +0100

    chore: replace org with joakimbeng

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/docker/machine/libmachine/drivers/plugin"
-	"github.com/scaleway/docker-machine-driver-scaleway/driver"
+	"github.com/joakimbeng/docker-machine-driver-scaleway/driver"
 )
 
 func main() {
